b78fa410518f13a3ef0b0e067f9345881d39245f
Stefan Wurzer <s52090@edu.campus02.at>
2022-09-16 19:28:32 +0200

    Fertig

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,7 +9,6 @@ public class HelloWorld {
         System.out.println("Zeile 3");
         System.out.println("Zeile 4");
         System.out.println("Zeile 5");
-        System.out.println("Zeile 6");
         //Steuerung D um eine Zeile zu duplizieren
     }
 }
