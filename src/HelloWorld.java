d95566d235b0c754d841de05b1e46069e831877c
Stefan Wurzer <s52090@edu.campus02.at>
2022-09-16 19:12:56 +0200

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    //main+TAB
+    //psvm+TAB
+    public static void main(String[] args) {
+        //sout+TAB
+        System.out.println("Hello World");
+        System.out.println("Welcome to KONF 2022!");
+        //mit Abkürzungen können hier einfach Begriffe eingefügt werden
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+        //Steuerung D um eine Zeile zu duplizieren
+    }
+}
